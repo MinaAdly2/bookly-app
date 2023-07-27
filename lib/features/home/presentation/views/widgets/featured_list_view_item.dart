bc9d8e9fd56851ba68dffaf36ab5d7c399cb9590
Mina Adly <mina2dly@gmial.com>
2023-07-28 01:51:22 +0300

    rename custom_list_view_item to featured_list_view_item

--- lib/features/home/presentation/views/widgets/featured_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/featured_list_view_item.dart
@@ -1,8 +1,8 @@
 import 'package:bookly/core/utils/app_asset_paths.dart';
 import 'package:flutter/material.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
