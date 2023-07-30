0297ca3cdb1fac399baff2c45c2b87b2b7db8901
Mina Adly <mina2dly@gmial.com>
2023-07-31 02:04:27 +0300

    rename featured item to custom book image

--- lib/features/home/presentation/views/widgets/custom_book_image.dart
+++ lib/features/home/presentation/views/widgets/custom_book_image.dart
@@ -1,8 +1,8 @@
 import 'package:bookly/core/utils/app_asset_paths.dart';
 import 'package:flutter/material.dart';
 
-class FeaturedItem extends StatelessWidget {
-  const FeaturedItem({super.key});
+class CustomBookImage extends StatelessWidget {
+  const CustomBookImage({super.key});
 
   @override
   Widget build(BuildContext context) {
