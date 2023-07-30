ec2e95b232cb774d199dab74e60fcc45c7080c04
Mina Adly <mina2dly@gmial.com>
2023-07-31 01:42:28 +0300

    create custom book details app bar

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,9 +1,11 @@
+import 'package:bookly/features/home/presentation/views/book_details_view.dart';
 import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
+  static const kBookDetailsView = '/bookDetailsView';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -14,6 +16,10 @@ abstract class AppRouter {
         path: kHomeView,
         builder: (context, state) => const HomeView(),
       ),
+      GoRoute(
+        path: kBookDetailsView,
+        builder: (context, state) => const BookDetailsView(),
+      )
     ],
   );
 }
--- lib/features/home/presentation/views/widgets/best_seller_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_item.dart
@@ -1,7 +1,9 @@
 import 'package:bookly/core/utils/app_asset_paths.dart';
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/app_styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BestSellerItem extends StatelessWidget {
   const BestSellerItem({super.key});
@@ -10,57 +12,62 @@ class BestSellerItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-      child: SizedBox(
-        height: 125,
-        child: Row(
-          children: [
-            AspectRatio(
-              aspectRatio: 2.5 / 4,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  image: const DecorationImage(
-                    image: AssetImage(AppAssetPaths.testImage),
-                    fit: BoxFit.fill,
+      child: GestureDetector(
+        onTap: () {
+          GoRouter.of(context).push(AppRouter.kBookDetailsView);
+        },
+        child: SizedBox(
+          height: 125,
+          child: Row(
+            children: [
+              AspectRatio(
+                aspectRatio: 2.5 / 4,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    image: const DecorationImage(
+                      image: AssetImage(AppAssetPaths.testImage),
+                      fit: BoxFit.fill,
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(width: 30),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    child: const Text(
-                      'Harry Potter and the Goblet of Fire',
-                      style: AppStyles.textSyle20,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                  const SizedBox(height: 3),
-                  const Text(
-                    'J.K. Rowling',
-                    style: AppStyles.textSyle14,
-                  ),
-                  const SizedBox(height: 3),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '19.99 €',
-                        style: AppStyles.textSyle20
-                            .copyWith(fontWeight: FontWeight.bold),
+              const SizedBox(width: 30),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      child: const Text(
+                        'Harry Potter and the Goblet of Fire',
+                        style: AppStyles.textSyle20,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
                       ),
-                      const BookRating(),
-                    ],
-                  ),
-                ],
+                    ),
+                    const SizedBox(height: 3),
+                    const Text(
+                      'J.K. Rowling',
+                      style: AppStyles.textSyle14,
+                    ),
+                    const SizedBox(height: 3),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          '19.99 €',
+                          style: AppStyles.textSyle20
+                              .copyWith(fontWeight: FontWeight.bold),
+                        ),
+                        const BookRating(),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
