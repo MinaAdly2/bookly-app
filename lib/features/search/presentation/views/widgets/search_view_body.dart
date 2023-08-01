dee6f64f2839f14387e79142653908a0db8b7cc5
Mina Adly <mina2dly@gmial.com>
2023-08-01 12:18:39 +0300

    search view is done

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,5 +1,7 @@
+import 'package:bookly/core/utils/app_styles.dart';
+import 'package:bookly/features/search/presentation/views/widgets/custom_search_text_field.dart';
+import 'package:bookly/features/search/presentation/views/widgets/search_result_list_view.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
@@ -7,41 +9,20 @@ class SearchViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CustomSearchTextField(),
+        SizedBox(height: 16),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: Text(
+            'Best Seller',
+            style: AppStyles.textStyle18,
+          ),
+        ),
+        SizedBox(height: 16),
+        Expanded(child: SearchResultListView()),
       ],
-    );
-  }
-}
-
-class CustomSearchTextField extends StatelessWidget {
-  const CustomSearchTextField({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-          border: buildOutlineInputBorder(),
-          enabledBorder: buildOutlineInputBorder(),
-          focusedBorder: buildOutlineInputBorder(),
-          hintText: 'Search',
-          suffixIcon: Opacity(
-            opacity: 0.8,
-            child: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                FontAwesomeIcons.magnifyingGlass,
-                size: 22,
-              ),
-            ),
-          )),
-    );
-  }
-
-  OutlineInputBorder buildOutlineInputBorder([color]) {
-    return OutlineInputBorder(
-      borderSide: BorderSide(color: color ?? Colors.white),
-      borderRadius: BorderRadius.circular(12),
     );
   }
 }
